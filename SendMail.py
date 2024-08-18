fca04a312c959a1d5cd5fdfb52bb4c38b19f69d5
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2024-08-18 16:27:28 +0530

    Update SendMail.py

--- SendMail.py
+++ SendMail.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import os
+import csv
 
 def ReadFile(directory):
     print("Reading Files from the directory")
@@ -19,7 +20,7 @@ def SendMail(fromaddr, toaddr, Files):
     print("Sending Email")
     msg = MIMEMultipart()
     msg['From'] = fromaddr
-    msg['To'] = toaddr
+    msg['To'] = ", ".join(toaddr)  # Convert list to string
     msg['Subject'] = "Baili Case Summarised"
     body = "These attachments are baili cases summarised"
 
@@ -28,7 +29,7 @@ def SendMail(fromaddr, toaddr, Files):
     try:
         for filepath in Files:
             filename = os.path.basename(filepath)
-            with open(filepath, "r") as attachment:
+            with open(filepath, "rb") as attachment:
                 p = MIMEBase('application', 'octet-stream')
                 p.set_payload(attachment.read())
 
@@ -37,7 +38,7 @@ def SendMail(fromaddr, toaddr, Files):
             msg.attach(p)
 
     except Exception as e:
-        print("Could not open file {e}")
+        print(f"Could not open file {e}")
 
     try:
         s = smtplib.SMTP('smtp.gmail.com', 587)
@@ -47,16 +48,36 @@ def SendMail(fromaddr, toaddr, Files):
         s.sendmail(fromaddr, toaddr, text)
         s.quit()
     except Exception as e:
-        print("Could not send email {e}")
+        print(f"Could not send email {e}")
 
     print("Ending Email function")
 
-def main():
-    fromaddr = "gangurdemaitreya@gmail.com"
-    toaddr = "maitreyagangurde@gmail.com"
-    directory = r"C:\Users\DELL\OneDrive\Desktop\Test\bailisummaryoutput"
+def Reciever(csv_path):
 
-    log_file_path = os.path.join(directory, 'EmailLogs.log')
+    try:
+        print("Reading EMAIL-id's from the csv file..")
+        mails = []
+
+        with open (csv_path, mode = 'r') as file:
+            csv_reader = csv.reader(file)
+
+            for row in csv_reader:
+                mails.append(row[0])
+
+        return mails
+    
+    except Exception as e:
+        print(f"Could not send email {e}")
+
+def main():
+    toaddr = []
+
+    fromaddr = "gangurdemaitreya@gmail.com"
+    csv_path = r'C:\Users\DELL\OneDrive\Desktop\FINAL\Emails.csv'
+    toaddr.extend(Reciever(csv_path))
+    directory = r"C:\Users\DELL\OneDrive\Desktop\FINAL\Bailisummaryoutput"
+
+    log_file_path = os.path.join(directory, 'EmailLog.log')
     os.makedirs(directory, exist_ok=True)
 
     processed_files = set()
